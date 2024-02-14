a9fef176a163dac84b38c9e9a3963e29f440335f
ayahhmeed <ayahhmeed@gmail.com>
2024-02-14 13:51:06 -0800

    privilegs

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,17 +1,3 @@
--- List privileges of MySQL users user_0d_1 and user_0d_2
-
-/* Create user user_0d_1 if it doesn't exist */
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
-/* Grant all privileges to user_0d_1 */
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
-/* Check privileges for user_0d_1 */
+-- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
-/* Create user user_0d_2 if it doesn't exist */
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
-/* Check privileges for user_0d_2 */
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
