79a73132bd1b281c7a3d68dacc56eee8df38186f
ayahhmeed <ayahhmeed@gmail.com>
2024-03-12 16:24:58 -0700

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor(w, h) {
-		if (w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		} else {
-			object.create(null);
-		}
-	}
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
 }
-
 
 module.exports = Rectangle;
